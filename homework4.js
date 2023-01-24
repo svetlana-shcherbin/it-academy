663750d4c9b1cb29860ffe7fc43bd0ffa311144f
Svetlana Shcherbin <svetlana.shcherbin@gmail.com>
2023-01-25 00:10:58 +0300

    task was fixed

--- homework4.js
+++ homework4.js
@@ -1,17 +1,18 @@
 const fs = require('fs-extra')
 
-const dir1 = '/users/svetlanashcherbin/Desktop/first'
-const file1 = '/users/svetlanashcherbin/Desktop/first/file.txt'
-const dir2 = '/users/svetlanashcherbin/Desktop/second'
-const file2 = '/users/svetlanashcherbin/Desktop/second/file.txt'
-const dir3 = '/users/svetlanashcherbin/Desktop/third'
+const dir1 = 'first'
+const desiredMode = 0o2775
+const file1 = 'first/file.txt'
+const dir2 = 'second'
+const file2 = 'second/file.txt'
+const dir3 = 'third'
 
-fs.ensureDirSync(dir1);
+fs.mkdirpSync(dir1, desiredMode);
 fs.ensureFileSync(file1);
-fs.ensureDirSync(dir2);
+fs.ensureDirSync(dir2, desiredMode);
 fs.ensureFileSync(file2);
 fs.moveSync(file1, file2, { overwrite: true });
-fs.ensureDirSync(dir3);
+fs.ensureDirSync(dir3, desiredMode);
 fs.copySync(dir2,dir3);
-fs.emptyDir(dir1,dir2,dir3 );
-fs.remove(dir1,dir2,dir3);
+fs.emptyDir(dir2);fs.emptyDir(dir3);
+fs.remove(dir1);fs.remove(dir2);fs.remove(dir3);
