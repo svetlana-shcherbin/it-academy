5c1a3ccc25b7809aea0d44403ba648596cecff2b
Svetlana Shcherbin <svetlana.shcherbin@gmail.com>
2023-07-24 20:00:30 +0300

    deleted old lessons

--- lesson-18/helpers/helpers.js
+++
@@ -1,8 +0,0 @@
-class Helpers {
-    static async getPriceWithoutCurrencySymbols(webElement) {
-        await webElement.map(async (element) => {
-            const priceWithCurrency = await element.getText();
-            return priceWithCurrency.slice(2);
-        });
-    }
-}
--- lesson-18/pageobjects/basePage.js
+++
@@ -1,7 +0,0 @@
-class BasePage {
-    async navigate(url) {
-        await browser.url(url);
-    }
-}
-
-module.exports = BasePage;
