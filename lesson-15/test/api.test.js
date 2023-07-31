92efd42a0c6830711b5a21f8f38c2a7f5aa6e779
Svetlana Shcherbin <svetlana.shcherbin@gmail.com>
2023-07-31 17:52:58 +0300

    added new tests

--- lesson-15/test/api.test.js
+++ lesson-15/test/api.test.js
@@ -48,7 +48,10 @@ describe("API POST a new user tests", function () {
   });
 
   test("POST should be valid json schema", async () => {
-    const result = await validator.validate(response.data, postAndPutUsersSchema);
+    const result = await validator.validate(
+      response.data,
+      postAndPutUsersSchema
+    );
     await expect(result.valid).toEqual(true);
   });
 });
@@ -63,17 +66,17 @@ describe("GET one user tests", function () {
         },
       }
     );
-  })
-  test("GET request should be 200", async () =>{
+  });
+  test("GET request should be 200", async () => {
     await expect(response.status).toEqual(200);
-  })
+  });
   test("GET should be valid json schema", async () => {
     const result = await validator.validate(response.data, getOneUserSchema);
     await expect(result.valid).toEqual(true);
   });
 });
 describe("API PUT a user", function () {
-    let response;
+  let response;
   beforeAll(async () => {
     response = await axios.put(
       "https://fakerestapi.azurewebsites.net/api/v1/Users/1",
@@ -95,7 +98,23 @@ describe("API PUT a user", function () {
   });
 
   test("PUT should be valid json schema", async () => {
-    const result = await validator.validate(response.data, postAndPutUsersSchema);
+    const result = await validator.validate(
+      response.data,
+      postAndPutUsersSchema
+    );
     await expect(result.valid).toEqual(true);
   });
-})
+});
+describe("API DELETE a user", function () {
+  test("DELETE request should be 200", async () => {
+    const response = await axios.delete(
+      "https://fakerestapi.azurewebsites.net/api/v1/Users/1",
+      {
+        headers: {
+          Accept: "*/*",
+        },
+      }
+    );
+    await expect(response.status).toEqual(200);
+  });
+});
