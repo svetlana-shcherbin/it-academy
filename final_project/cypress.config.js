e01ee25481a1db0515a4b3f9d54e4e18d939a52d
Svetlana Shcherbin <svetlana.shcherbin@gmail.com>
2023-08-31 19:38:03 +0300

    added reporter

--- final_project/cypress.config.js
+++ final_project/cypress.config.js
@@ -2,13 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    reporter: 'reports/custom.js',
     specPattern: 	'cypress/test/**/*.cy.{js,jsx,ts,tsx}',
     pageLoadTimeout:6000,
     screenshotOnRunFailure: true,
     screenshotsFolder: 'cypress/screenshots',
+    video:false,
     trashAssetsBeforeRuns: true
   },
 });
